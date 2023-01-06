320717e59c10df73d36a4714cbcd8089979a4a72
Sergey Gulaga <ry6ens@gmail.com>
2023-01-06 20:02:56 +0200

    update summary

--- src/api/transactions.js
+++ src/api/transactions.js
@@ -45,5 +45,6 @@ export const axiosGetTransactionsByMonth = async userData => {
 //Get summary by year
 export const axiosGetSummary = async userData => {
   const { data } = await instance.post('api/transaction/summary', userData);
+  console.log(data);
   return data;
 };
